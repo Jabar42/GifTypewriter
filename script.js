f308c7ed7eb586c3f77b5cfdb27395b7d6a92a50
jabar42 <jaimebarbudomier@gmail.com>
2024-05-22 18:13:20 -0300

    Se agrego la libreria GIF

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+const gif = new GIF({
+    workers: 2,
+    quality: 10,
+    width: 341,
+    height: 204
+});
+
 document.getElementById('generateBtn').addEventListener('click', () => {
     const textInput = document.getElementById('textInput').value;
     const preview = document.getElementById('animationPreview');
+    const gifPreview = document.getElementById('generatedGif');
     preview.innerHTML = '';
-    
+    gifPreview.src = '';
+
     let index = 0;
     const cursor = document.createElement('span');
     cursor.classList.add('cursor');
@@ -19,9 +28,17 @@ document.getElementById('generateBtn').addEventListener('click', () => {
             const span = document.createElement('span');
             span.textContent = textInput[index];
             preview.insertBefore(span, cursor);
+            gif.addFrame(preview, {delay: 100}); // Añadir el frame al gif
             index++;
             setTimeout(typeWriter, 100); // Ajustar la velocidad según sea necesario
+        } else {
+            gif.render();
         }
     };
     typeWriter();
+});
+
+gif.on('finished', function(blob) {
+    const gifUrl = URL.createObjectURL(blob);
+    document.getElementById('generatedGif').src = gifUrl;
 });
